381af773a3528a167c07f6cc673156bb847f2827
gabbor <gabriele.atria@gmail.com>
2020-05-25 17:24:13 +0200

    script for feature images

---
+++ scripts/generate_feature_images.sh
@@ -0,0 +1,13 @@
+./bin/yscenetrace tests/01_bathroom/bathroom.json -o out/bathroom/bathroom_normal.hdr -s 64 -b 8 -r 1280 -t normal_denoiser
+./bin/yscenetrace tests/01_bathroom/bathroom.json -o out/bathroom/bathroom_albedo.hdr -s 128 -b 8 -r 1280 -t albedo
+
+./bin/yscenetrace tests/02_bedroom/bedroom.json -o out/bedroom/bedroom_normal.hdr -s 64 -b 8 -r 1280 -t normal_denoiser
+./bin/yscenetrace tests/02_bedroom/bedroom.json -o out/bedroom/bedroom_albedo.hdr -s 128 -b 8 -r 1280 -t albedo
+
+./bin/yscenetrace tests/03_classroom/classroom.json -o out/classroom/classroom_normal.hdr -s 64 -b 8 -r 1280 -t normal_denoiser
+./bin/yscenetrace tests/03_classroom/classroom.json -o out/classroom/classroom_albedo.hdr -s 128 -b 8 -r 1280 -t albedo
+
+./bin/yscenetrace tests/04_kitchen/kitchen.json -o out/kitchen/kitchen_normal.hdr -s 64 -b 8 -r 1280 -t normal_denoiser
+./bin/yscenetrace tests/04_kitchen/kitchen.json -o out/kitchen/kitchen_albedo.hdr -s 128 -b 8 -r 1280 -t albedo
+
+
